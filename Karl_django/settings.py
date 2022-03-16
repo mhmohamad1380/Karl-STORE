7382cef29f91c386303abcb55b87a071560fe3e4
Mohammad <76651930+mhmohamad1380@users.noreply.github.com>
2022-03-16 23:22:41 +0330

    Update settings.py

--- Karl_django/settings.py
+++ Karl_django/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-&_3=0@3#-mw9r^9kuzth%@imrxkgusyx#r_534goh%#8yj%tdi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
